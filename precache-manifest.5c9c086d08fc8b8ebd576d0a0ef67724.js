751296f6408e7015f88245f9ba1166b3f12250f4
Pietrzi <pietrzi@op.pl>
2020-01-14 20:20:26 +0100

    Updates

---
+++ precache-manifest.5c9c086d08fc8b8ebd576d0a0ef67724.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6404555269e6444d2a7431703795e8",
+    "url": "/cocktails-app/index.html"
+  },
+  {
+    "revision": "3a52cec86dbda83ea230",
+    "url": "/cocktails-app/static/css/main.7fb023de.chunk.css"
+  },
+  {
+    "revision": "7f5e0f1a0e3e4df63768",
+    "url": "/cocktails-app/static/js/2.740c5037.chunk.js"
+  },
+  {
+    "revision": "3a52cec86dbda83ea230",
+    "url": "/cocktails-app/static/js/main.36c22435.chunk.js"
+  },
+  {
+    "revision": "5fe2f1cd3f0066f12139",
+    "url": "/cocktails-app/static/js/runtime~main.2770cb87.js"
+  },
+  {
+    "revision": "61bdbd4688e61bd2a01909f6cca6305c",
+    "url": "/cocktails-app/static/media/drinks1.61bdbd46.jpg"
+  }
+]);
